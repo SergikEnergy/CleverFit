c520a33aa034af85a47efcc90a0d92e203a4d8d8
SergEnergy <kis8883244@gmail.com>
2024-03-01 18:35:14 +0300

    feat: add empty feedback

---
+++ src/components/withoutComments/WithoutComments.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+
+import { Button } from 'antd';
+import classes from './WithoutComments.module.css';
+
+export const WithoutComments: FC = () => {
+    const handleAddFeedback = () => {
+        //
+    };
+
+    return (
+        <div className={classes['feedback__empty']}>
+            <div className={classes.wrapper}>
+                <div className={classes.description}>
+                    <div className={classes.title}>Оставьте свой отзыв первым</div>
+                    <div className={classes.content}>
+                        Вы можете быть первым, кто оставит отзыв об этом фитнесс приложении.
+                        <br className={classes.breaks} />
+                        Поделитесь своим мнением и опытом с другими пользователями,
+                        <br className={classes.breaks} /> и&nbsp;помогите им сделать правильный
+                        выбор.
+                    </div>
+                </div>
+                <div className={classes.button}>
+                    <Button block htmlType='button' type='primary' onClick={handleAddFeedback}>
+                        Написать отзыв
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/pages/feedbacks-page/feedbacks-page.tsx
+++ src/pages/feedbacks-page/feedbacks-page.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import { BaseMainFeedbacksLayout } from '@pages/baseMainFeedbacks';
+import { WithoutComments } from '@components/withoutComments';
+
 import classes from './feedbacks-page.module.css';
 
 export const FeedbacksPage: FC = () => {
     return (
         <BaseMainFeedbacksLayout isFeedbackPage={true}>
-            Welcome to feedbacks page
+            <WithoutComments />
         </BaseMainFeedbacksLayout>
     );
 };
