1a0f3b0f97bdd15fee05b97e20033d1a6038d505
SergEnergy <kis8883244@gmail.com>
2024-03-04 13:45:44 +0300

    fix: replace toSorted on sort to pass tests

--- src/components/feedbacks/Feedbacks.tsx
+++ src/components/feedbacks/Feedbacks.tsx
@@ -18,13 +18,13 @@ export const Feedbacks: FC<IFeedbacksProps> = ({ feedbacks }) => {
     const [textButton, setTextButton] = useState<string>(spreadCommentText);
     const [limit, setLimit] = useState(4);
 
-    const sortedFeedbacks = Array.isArray(feedbacks)
-        ? feedbacks.toSorted((item1: IFeedbackResponse, item2: IFeedbackResponse) => {
-              const dataItem1 = new Date(item1.createdAt);
-              const dataItem2 = new Date(item2.createdAt);
-              return dataItem1.getTime() - dataItem2.getTime();
-          })
-        : [];
+    if (Array.isArray(feedbacks)) {
+        feedbacks.toSorted((item1: IFeedbackResponse, item2: IFeedbackResponse) => {
+            const dataItem1 = new Date(item1.createdAt);
+            const dataItem2 = new Date(item2.createdAt);
+            return dataItem1.getTime() - dataItem2.getTime();
+        });
+    }
 
     const handleCreateButtonClick = () => {
         setNode(<NewFeedback />);
@@ -41,7 +41,7 @@ export const Feedbacks: FC<IFeedbacksProps> = ({ feedbacks }) => {
 
     return (
         <div className={classes.feedbacks}>
-            <FeedbacksList feedbacks={sortedFeedbacks} limit={limit} />
+            <FeedbacksList feedbacks={feedbacks ?? []} limit={limit} />
             <div className={classes.navigation}>
                 <Button
                     data-test-id='write-review'
