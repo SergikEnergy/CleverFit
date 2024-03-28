afa6f6a8df91697cdb9a1a00b8ad8208163d5a43
SergEnergy <kis8883244@gmail.com>
2024-03-28 17:28:20 +0300

    feat: fix dangle underscore rules at eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,13 @@ module.exports = {
     ],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'no-underscore-dangle': [
+            'error',
+            {
+                allowAfterThis: true,
+                allowed: ['_id'],
+            },
+        ],
         'import/no-absolute-path': 'off',
         'react/react-in-jsx-scope': 'off',
         'import/no-extraneous-dependencies': [
